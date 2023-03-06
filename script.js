6ad0a2d6242c8f87df30d872df9faa73b1e0c96a
shalivahanvishwajit <88126733+shalivahanvishwajit@users.noreply.github.com>
2023-03-06 10:22:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+// // const joke = document.getElementById("joke");
+// // const btn = document.getElementById("btn");
+// // const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
+
+// // let getJoke = () => {
+// //     jokeContainer.classList.remove("fade");
+// //     fetch(url)
+// //     .then(data => data.json())
+// //     .then(item =>{
+// //         jokeContainer.textContent = `${item.joke}`;
+// //         jokeContainer.classList.add("fade");
+// //     });
+// // }
+// // btn.addEventListener("click",getJoke);
+// // getJoke();
+
+
+
+
+
+// 	const joke = document.getElementById('joke');
+// {/* // const dog_btn = document.getElementById('dog_btn'); */}
+// const btn = document.getElementById('btn');
+// // const dog_result = document.getElementById('dog_result');
+
+
+// // dog_btn.addEventListener('click', getRandomDog);
+
+// function getRandomCat() {
+// 	fetch('https://v2.jokeapi.dev/joke/Any')
+// 		.then(data => data.json())
+// 		.then(item =>
+		
+// 		 joke.innerHTML( '${item.setup}' ));
+// 		}
+// btn.addEventListener('click', getRandomCat);
+// getRandomCat()
